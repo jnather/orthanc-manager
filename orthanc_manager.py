ced3464f5470aa7d536ddde960c807a8bd0ef4c6
jnather <julio.nather@gmail.com>
2024-08-29 15:39:54 -0300

    a

--- orthanc_manager.py
+++ orthanc_manager.py
@@ -119,4 +119,8 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=5000, ssl_context=('cert/cert.pem', 'cert/key.pem'), debug=True)
+
